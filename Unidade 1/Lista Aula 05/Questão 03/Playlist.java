b9d5609baf74c270d1e9fd0987aecf2e727a53cb
Carlos Eduardo <30722229+CarlosEduard0@users.noreply.github.com>
2018-08-14 10:57:53 -0300

    Inicia a implementação da Playlist

--- Unidade 1/Lista Aula 05/Questão 03/Playlist.java
+++ Unidade 1/Lista Aula 05/Questão 03/Playlist.java
@@ -1,3 +1,36 @@
+import java.util.ArrayList;
+
 public class Playlist {
-	
+
+    private ArrayList<String> songs;
+
+    public Playlist() {
+        this.songs = new ArrayList<String>();
+    }
+
+    public void addFile(String music) {
+        this.songs.add(music);
+    }
+
+    public int getNumberOfFiles() {
+        return this.songs.size();
+    }
+
+    public void listFiles() {
+        for(int i = 0; i < this.songs.size(); i++) {
+            System.out.println(i + " - " + this.songs.get(i));
+        }
+    }
+
+    public void playAll() {}
+
+    public void playFile(String song) {}
+
+    public void removeFile(int index) {
+        this.songs.remove(index);
+    }
+
+    public void removeFile(String music) {
+        this.songs.remove(music);
+    }
 }
